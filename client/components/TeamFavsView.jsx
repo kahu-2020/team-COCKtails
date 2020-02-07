c58fe6f52ff9a6c7b9fbda443bdb687dfdcb34c1
student <student@eda-bc-04.localdomain>
2020-02-07 14:49:33 +1300

    team favourites button

---
+++ client/components/TeamFavsView.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function TeamFavsView() {
+  return (
+    <div>
+      <h1>Team Favourites!</h1>
+      <h2>Scott - Old Fashioned</h2>
+      <h2>Josh- Mojito</h2>
+      <h2>Ethan- Lightning Joe Crow</h2>
+      <h2>Gareth - Singapore Sling</h2>
+    </div>
+  )
+}
+
+export default TeamFavsView
